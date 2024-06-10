6c54954ea164cb8f9bc79f35c7b8d80d7f0c7fe7
wth is a pull request <117620492+PalaboiCarti@users.noreply.github.com>
2024-06-10 18:19:58 +0800

    For whoever tf may need this
    Aral nang mabuti mga chong ahaha

---
+++ Jun10_2024.cs
@@ -0,0 +1,81 @@
+using System;
+
+public class Cigarette
+{
+    //attributes
+    private string name;
+    public string propName{
+        get{return name;}
+        set{name = value;}
+    }
+    
+    private string brand;
+    public string propBrand{
+        get{return brand;}
+        set{brand = value;}
+    }
+    private string quality;
+    public string propQuality{
+        get{return quality;}
+        set{quality = value;}
+    }
+    
+    private int amount;
+    public int propAmount{
+        get{return amount;}
+        set{amount = value;}
+    }
+    
+    private int price;
+    public int propPrice{
+        get{return price;}
+        set{price = value;}
+    }
+    
+    private bool cancer;
+    public bool propCancer{
+        get{return cancer;}
+        set{cancer = value;}
+    }
+    
+    //constructor
+    public Cigarette(string Name, string Brand, string Quality, int Amount, int Price, bool Cancer)
+    {
+        name = Name;
+        brand = Brand;
+        quality = Quality;
+        amount = Amount;
+        price = Price;
+        cancer = Cancer;
+    }
+    
+    //method
+    public void info(){
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Brand: {brand}");
+        Console.WriteLine($"Okey ba to? {quality} yan");
+        Console.WriteLine($"Ilan sa isang kaha? {amount} tol");
+        Console.WriteLine($"Magkano yan? {price} lang yan");
+        if(cancer == true){
+            Console.WriteLine("Okey na sana kaso magkaka cancer ka neto eh");
+        }else{
+            Console.WriteLine("Efas to men");
+        }
+        
+    }
+}
+
+class Program{
+    static void Main(){
+        Cigarette MarlboroRed = new Cigarette("Marlboro Red", "Marlboro", "Goodshit", 20, 180, true);
+        
+        MarlboroRed.propName = "Marlboro Red";
+        MarlboroRed.propBrand = "Marlboro";
+        MarlboroRed.propQuality = "Goodshit";
+        MarlboroRed.propAmount = 20;
+        MarlboroRed.propPrice = 180;
+        MarlboroRed.propCancer = true;
+        
+        MarlboroRed.info();
+    }
+}
